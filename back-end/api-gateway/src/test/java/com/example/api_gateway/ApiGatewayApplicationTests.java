80f38ae8e213a33a33d8decf85d26700530fb4c3
timothe-git <timothe.paquet@outlook.com>
2025-11-27 11:29:46 +0100

    Basic gateway with spring following the guide on spring.io

--- back-end/api-gateway/src/test/java/com/example/api_gateway/ApiGatewayApplicationTests.java
+++ back-end/api-gateway/src/test/java/com/example/api_gateway/ApiGatewayApplicationTests.java
@@ -1,13 +1,49 @@
 package com.example.api_gateway;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+    properties = {"httpbin=http://localhost:${wiremock.server.port}"})
+@AutoConfigureWireMock(port = 0)
 class ApiGatewayApplicationTests {
 
+	@Autowired
+	private WebTestClient webClient;
+
 	@Test
-	void contextLoads() {
+	public void contextLoads() throws Exception {
+		//Stubs
+		stubFor(get(urlEqualTo("/get"))
+			.willReturn(aResponse()
+			.withBody("{\"headers\":{\"Hello\":\"World\"}}")
+			.withHeader("Content-Type", "application/json")));
+		stubFor(get(urlEqualTo("/delay/3"))
+		.willReturn(aResponse()
+			.withBody("no fallback")
+			.withFixedDelay(3000)));
+
+		webClient
+		.get().uri("/get")
+		.exchange()
+		.expectStatus().isOk()
+		.expectBody()
+		.jsonPath("$.headers.Hello").isEqualTo("World");
+
+		webClient
+		.get().uri("/delay/3")
+		.header("Host", "www.circuitbreaker.com")
+		.exchange()
+		.expectStatus().isOk()
+		.expectBody()
+		.consumeWith(
+			response -> assertThat(response.getResponseBody()).isEqualTo("fallback".getBytes()));
 	}
 
 }
