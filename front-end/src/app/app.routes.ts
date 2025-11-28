65f0d5285ebdd402351d2ab8c3b6b0c2ddb43c53
timothe-git <timothe.paquet@outlook.com>
2025-11-28 22:35:37 +0100

    Add tailwindcss and the components home, login, navbar, not-found and user. First UI for navbar,login and home

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { UserComponent } from './user/user.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { LoginComponent } from './login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        title: 'Home paage',
+        component: HomeComponent,
+    },
+    {
+        path: 'user',
+        title: 'User paage',
+        component: UserComponent,
+    },
+    {
+        path: 'login',
+        title: 'Login paage',
+        component: LoginComponent,
+    },
+    {
+        path: '**',
+        title: 'Not found paage',
+        component: NotFoundComponent,
+    },
+];
